7bac07a7c770b1f756f91846919f5f10536ad7df
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2021-04-19 21:38:01 -0600

    Added vue axios

--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 Vue.config.productionTip = false
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(VueAxios, axios)
+
 
 new Vue({
   router,
