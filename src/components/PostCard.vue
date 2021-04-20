b245278d2e55c3d06ec83d9c064cc4fe5156f24f
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2021-04-19 22:22:35 -0600

    Added custom component PostCard

---
+++ src/components/PostCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-header">
+                {{title}}
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                <p>{{content}}</p>
+                <footer class="blockquote-footer">Publicado el: {{publishedDate}}</footer>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PostCard',
+  props: {
+    title: String,
+    content: String,
+    publishedDate: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .card {
+        margin: 50px auto;
+        width: 80%;
+        text-align: left;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    Pagina en blanco
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <PostCard 
+      title="Titulo de la nota" 
+      content="Contenido de la nota" 
+      publishedDate="23-423-4-234-324"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import PostCard from '@/components/PostCard.vue'
 
 export default {
   name: 'Home',
   components: {
     // HelloWorld
+    PostCard
   }
 }
 </script>
