29f745ab12aadefc46133aaf9bfb4527e6cc5b11
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2021-05-02 12:34:09 -0600

    Added Datos view

---
+++ src/views/Datos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Datos',
+    components: {
+        
+    },
+    data() {
+        return {
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
